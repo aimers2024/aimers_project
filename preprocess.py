685631712f386f9b661b394d33535483be14c8eb
안성원 <ans2568@gmail.com>
2024-02-13 19:29:29 +0900

    Add code.ipynb, config.json, preprocess.py

---
+++ preprocess.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import pickle
+from category_encoders import OneHotEncoder, OrdinalEncoder
+
+class DataPreprocessor:
+    def __init__(self, preprocess_columns, train_mode=True):
+        self.preprocess_columns = preprocess_columns
+        self.train_mode = train_mode
+        self.enc_ordinal = None
+        self.frequency_mappings = {}
+        self.enc_onehot = None
+        if not train_mode:
+            self.load_encoders()
+    def preprocess(self,df):
+        #Preprocessing
+        ##id_strategic_ver,it_strategic_ver 병합
+        df['id_strategic_ver'] = df['id_strategic_ver'].fillna(0)
+        df['it_strategic_ver'] = df['it_strategic_ver'].fillna(0)
+        df['idit_strategic_ver'] = df['id_strategic_ver'] + df['it_strategic_ver']
+
+        ##product_category가 null인 값 중 product_subcategory 값이 null이 아니면 해당 값 가져오기
+        df.loc[df['product_category'].isnull() & df['product_subcategory'].notnull(), 'product_category'] = df['product_subcategory']
+
+        #customer contry 열에서 나라만 뽑아보기
+        df['customer_country'] = df["customer_country"].str.split('/').str[2]
+
+        #Drop
+        df = df.drop(self.preprocess_columns['drop_column'], axis=1)
+
+
+        #Fill-Null
+        for column in self.preprocess_columns['null_integer']:
+            df[column] = df[column].fillna(0)
+            
+        for column in self.preprocess_columns['null_string']:
+            df[column] = df[column].fillna('others')
+            
+
+        #Group
+        for column in self.preprocess_columns['Grouping']:
+            df.loc[df[column].isin(self.preprocess_columns["grouping_element"]), column] = 'others'
+
+        ##value counts가 1인 값들 전부다 others로 바꾸기
+        for column in self.preprocess_columns['Categorical']:
+            value_counts = df[column].value_counts()
+
+            ###빈도수가 1인 값들 찾기
+            values_to_replace = value_counts[value_counts == 1].index.tolist()
+
+            ###빈도수가 1인 값들을 'others'로 치환
+            df[column] = df[column].replace(values_to_replace, 'others')
+        return df
+    
+    def fit_transform(self, df):
+        df = self.preprocess(df)
+        # Ordinal encoding
+        self.enc_ordinal = OrdinalEncoder(cols=self.preprocess_columns['Ordinal'])
+        df = self.enc_ordinal.fit_transform(df)
+       
+        # Frequency encoding
+        for column in self.preprocess_columns['Frequency']:
+            frequency = df.groupby(column).size() / len(df)
+            self.frequency_mappings[column] = frequency
+            df[column] = df[column].apply(lambda x: self.frequency_mappings[column].get(x, 0))
+        
+        # One-hot encoding
+        self.enc_onehot = OneHotEncoder(cols=self.preprocess_columns['OneHot'], use_cat_names=True)
+        df = self.enc_onehot.fit_transform(df)
+        
+        self.save_encoders()
+        return df
+
+    def transform(self, df):
+        df = self.preprocess(df)
+        
+        # 인코더 로드는 __init__에서 수행됨
+        
+        # Ordinal encoding
+        df = self.enc_ordinal.transform(df)
+        
+        # One-hot encoding
+        df = self.enc_onehot.transform(df)
+        
+        return df
+
+    def save_encoders(self):
+        with open('enc_ordinal.pkl', 'wb') as file:
+            pickle.dump(self.enc_ordinal, file)
+        with open('frequency_mappings.pkl', 'wb') as file:
+            pickle.dump(self.frequency_mappings, file)
+        with open('enc_onehot.pkl', 'wb') as file:
+            pickle.dump(self.enc_onehot, file)
+    
+    def load_encoders(self):
+        with open('enc_ordinal.pkl', 'rb') as file:
+            self.enc_ordinal = pickle.load(file)
+        with open('frequency_mappings.pkl', 'rb') as file:
+            self.frequency_mappings = pickle.load(file)
+        with open('enc_onehot.pkl', 'rb') as file:
+            self.enc_onehot = pickle.load(file)
